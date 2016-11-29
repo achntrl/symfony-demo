d9320a0c8128de4601932cc8254f13f00e6a5517
Alexandre Chaintreuil <alexandre.chaintreuil@gmail.com>
2016-11-29 10:08:29 +0100

    Add tests

--- tests/AppBundle/Controller/BlogControllerTest.php
+++ tests/AppBundle/Controller/BlogControllerTest.php
@@ -11,8 +11,14 @@
 
 namespace Tests\AppBundle\Controller;
 
-use AppBundle\Entity\Post;
+use Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use AppBundle\Entity\Post;
+use AppBundle\Entity\Comment;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Component\HttpFoundation\Request;
+
 
 /**
  * Functional test for the controllers defined inside BlogController.
@@ -23,6 +29,7 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
  *
  *     $ cd your-symfony-project/
  *     $ phpunit -c app
+ *
  */
 class BlogControllerTest extends WebTestCase
 {
@@ -36,5 +43,39 @@ class BlogControllerTest extends WebTestCase
             $crawler->filter('article.post'),
             'The homepage displays the right number of posts.'
         );
+
+    }
+
+    public function testTitle()
+    {
+        // Parse all the articles from the frontpage
+        $client = static::createClient();
+        $blogCrawler = $client->request('GET', '/en/blog/');
+        $links = $blogCrawler->filter('h2 > a');
+        for ($i = 0 ; $i < count($links) ; $i++)
+        {
+            // For each article, compare the title to the first h1 header
+            $url =  $links->eq($i)->link()->getUri();
+            $articleCrawler = $client->request('GET', $url);
+            $h1 = $articleCrawler->filter('h1')->eq(0)->text();
+            $title = $client->getRequest()->attributes->get('post')->getTitle();
+            $this->assertEquals($title, $h1);
+        }
+    }
+
+    public function testComments()
+    {
+        $client = static::createClient();
+        $blogCrawler = $client->request('GET', '/en/blog/');
+        $links = $blogCrawler->filter('h2 > a');
+        for ($i = 0 ; $i < count($links) ; $i++)
+        {
+            $url =  $links->eq($i)->link()->getUri();
+            $articleCrawler = $client->request('GET', $url);
+            // Comments or No comments message
+            $commentCounter = count($articleCrawler->filterXPath("//body/descendant::div[@class='row post-comment']")) +
+                             count($articleCrawler->filterXPath("//body/descendant::div[@class='post-comment']"));
+            $this->assertGreaterThan(0, $commentCounter);
+        }
     }
 }
